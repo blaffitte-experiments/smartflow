cc78baa0cfe2246ad1a04d9e5bb3d25ba081ef48
Benoit Laffitte <b.laffitte@cat-amania.com>
2024-11-26 21:52:09 +0100

    Ajout du script

---
+++ genere_commit.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Nombre de commits à générer
+NBR_COMMITS=10
+
+# Array de messages de commit
+commit_messages=(
+    "Ajout de la fonctionnalité X"
+    "Correction du bug Y"
+    "Amélioration de la performance de Z"
+    "Refactorisation du code"
+    "Mise à jour de la documentation"
+    "Nettoyage du code"
+    "Ajout de tests unitaires"
+    "Mise à jour des dépendances"
+    "Correction des fautes de frappe"
+    "Amélioration de l'interface utilisateur"
+)
+
+# Boucle pour générer les commits
+for i in $(seq 1 $NBR_COMMITS)
+do
+    # Créer un fichier temporaire pour chaque commit
+    touch "tempfile_$i.txt"
+    git add "tempfile_$i.txt"
+    
+    # Sélectionner un message de commit aléatoire
+    commit_message=${commit_messages[$RANDOM % ${#commit_messages[@]}]}
+    
+    # Créer le commit
+    git commit -m "$commit_message"
+    
+    echo "Commit $i: $commit_message"
+done
+
+echo "Génération de $NBR_COMMITS commits terminée."
